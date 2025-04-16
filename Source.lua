141c4e7dc92ad9864d377e915e846d064f97700e
FuturisticSearch <vokounvojta03@gmail.com>
2025-04-16 17:39:26 +0200

    Delete Source.lua

--- Source.lua
+++
@@ -1,7 +0,0 @@
-local mainName='QuirkyCMD Admin'
-local testingName='QCMD Testing'
-local adminName='QCMD'
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ltseverydayyou/Nameless-Admin/refs/heads/main/NA%20testing.lua"))()
-local mainName='QuirkyCMD Admin'
-local testingName='QCMD Testing'
-local adminName='QCMD'
